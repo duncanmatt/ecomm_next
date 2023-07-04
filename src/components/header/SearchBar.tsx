e1d6404582860143046424255daff944341d4dee
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2023-07-04 16:07:12 -0400

    update

--- src/components/header/SearchBar.tsx
+++ src/components/header/SearchBar.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const SearchBar = () => {
-	const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
 
-	return <div>SEARCH...</div>;
+  return <div>SEARCH...</div>;
 };
 
 export default SearchBar;
