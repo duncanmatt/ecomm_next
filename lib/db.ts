8ed541f033e28d1d0d33a4df48de81f8ebae4af9
duncanmatt <117763103+duncanmatt@users.noreply.github.com>
2023-07-11 15:56:41 -0400

    lucia

--- lib/db.ts
+++
@@ -1,5 +0,0 @@
-import postgres from 'postgres';
-
-const sql = postgres(process.env.DB_URL as string);
-
-export default sql;
---
+++ lucia.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="lucia" />
+declare namespace Lucia {
+	type Auth = import('./lib/lucia').Auth;
+	type UserAttributes = {};
+}
