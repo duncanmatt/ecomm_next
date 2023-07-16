60cfe832e9759cf23e39295f55e2bb672c746b49
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2023-07-16 05:55:31 -0400

    remove header on scroll down

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,47 @@
+import { useState, useEffect } from 'react';
 import Mobile from './Mobile';
 import Desktop from './Desktop';
 import React from 'react';
 
 const Header = () => {
+  const [scrollDir, setScrollDir] = useState('down');
+
+  useEffect(() => {
+    const threshold = 3;
+    let lastY = window.pageYOffset;
+    let ticking = false;
+
+    const updateScrollDir = () => {
+      const y = window.pageYOffset;
+
+      if (Math.abs(y - lastY) < threshold) {
+        ticking = false;
+        return;
+      }
+      setScrollDir(y > lastY ? 'down' : 'up');
+      lastY = y > 0 ? y : 0;
+      ticking = false;
+    };
+
+    const onScroll = () => {
+      if (!ticking) {
+        window.requestAnimationFrame(updateScrollDir);
+        ticking = true;
+      }
+    };
+
+    window.addEventListener('scroll', onScroll);
+
+    return () => window.removeEventListener('scroll', onScroll);
+  }, [scrollDir]);
+
   return (
     <>
-      <header className='h-60 z-[60] border-b fixed top-0 right-0 left-0 border-g'>
+      <header
+        className={`h-60 ${
+          scrollDir === 'up' ? 'block' : 'hidden'
+        } z-[60] border-b fixed top-0 right-0 left-0 border-g`}
+      >
         <div className='relative bg-scroll px-1rem md:hidden h-full'>
           <Mobile />
         </div>
