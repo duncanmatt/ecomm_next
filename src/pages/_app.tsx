abd913b8a90d1792692f162490cffa24e5031d15
duncanmatt <117763103+duncanmatt@users.noreply.github.com>
2023-07-17 21:58:42 -0400

    redux
    only cart slice for now

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
+import { Providers } from '../../lib/providers';
 
 export default function App({
 	Component,
 	pageProps: { session, ...pageProps },
 }: AppProps) {
 	return (
-		<SessionProvider session={session}>
-			<Component {...pageProps} />
-		</SessionProvider>
+		<Providers>
+			<SessionProvider session={session}>
+				<Component {...pageProps} />
+			</SessionProvider>
+		</Providers>
 	);
 }
