ab62c91a28e2b4a8c2ad86d097125b99cfe7e85e
duncanmatt <117763103+duncanmatt@users.noreply.github.com>
2023-07-20 16:06:35 -0400

    cart items selector

--- lib/redux/slices/cartSlice/selectors.ts
+++ lib/redux/slices/cartSlice/selectors.ts
@@ -1,3 +1,4 @@
 import type { ReduxState } from '../../store';
 
 export const cartCount = (state: ReduxState) => state.cart.items.length;
+export const cartItems = (state: ReduxState) => state.cart.items;
