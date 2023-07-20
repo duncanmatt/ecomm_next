5e0f294468173a0686901d5a4ac50c19115d657d
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2023-07-20 19:57:46 -0400

    price and items displayed  on cart

--- lib/redux/slices/cartSlice/selectors.ts
+++ lib/redux/slices/cartSlice/selectors.ts
@@ -2,3 +2,4 @@ import type { ReduxState } from '../../store';
 
 export const cartCount = (state: ReduxState) => state.cart.items.length;
 export const cartItems = (state: ReduxState) => state.cart.items;
+export const cartTotal = (state: ReduxState) => state.cart.totalPrice;
--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -1,15 +1,33 @@
 import { useState } from 'react';
 import { useSelector } from '../../../lib/redux/store';
-import { cartCount } from '../../../lib/redux/slices/cartSlice/selectors';
+import {
+  cartCount,
+  cartTotal,
+  cartItems,
+} from '../../../lib/redux/slices/cartSlice/selectors';
 
 const Cart = () => {
-	const count = useSelector(cartCount);
-	return (
-		<div>
-			<div>Cart</div>
-			<span>{count}</span>
-		</div>
-	);
+  const count = useSelector(cartCount);
+  const price = useSelector(cartTotal);
+  const items = useSelector(cartItems);
+
+  return (
+    <div>
+      <div>Cart</div>
+      <span>{count}</span>
+      <br />
+      <span>{price}</span>
+      <div>
+        <ul>
+          {items.map((item, index) => (
+            <li key={index}>
+              <span>{item.name}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
