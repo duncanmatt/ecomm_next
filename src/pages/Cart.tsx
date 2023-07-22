15b469a7ee61cc3f3ecb9153057c7ed2774344ac
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2023-07-21 21:21:47 -0400

    cart content

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,8 +1,47 @@
 import Layout from '@/components/Layout';
-import React from 'react';
+import { useSelector, useDispatch } from '../../lib/redux/store';
+import {
+  cartTotal,
+  cartItems,
+} from '../../lib/redux/slices/cartSlice/selectors';
+import { CartItem } from '../../interfaces';
+import Image from 'next/image';
 
 const Cart = () => {
-	return <Layout>CART</Layout>;
+  const dispath = useDispatch();
+  const price = useSelector(cartTotal);
+  const items = useSelector(cartItems);
+
+  return (
+    <Layout>
+      <div>
+        <h2 className='font-bold'>Cart</h2>
+        <div>
+          <ul>
+            {items.map((item: CartItem, index: number) => (
+              <li key={index}>
+                <div className='flex flex-row'>
+                  <Image
+                    alt={item.name}
+                    src={item.imgUrl}
+                    width={168}
+                    height={227}
+                  />
+                  <div className='flex flex-col justify-around'>
+                    <span>{item.name}</span>
+                    <span>
+                      ${item.price}({item.qty})
+                    </span>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>${price}</div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Cart;
