f232c98294e46722ce90e5e379aafb3bc734abd3
duncanmatt <117763103+duncanmatt@users.noreply.github.com>
2023-07-23 17:38:07 -0400

    remove console.log

--- src/pages/products/product.tsx
+++ src/pages/products/product.tsx
@@ -2,9 +2,9 @@ import client from '../../../lib/db';
 import Layout from '@/components/Layout';
 import Image from 'next/image';
 import type {
-  GetServerSidePropsContext,
-  GetServerSideProps,
-  InferGetServerSidePropsType,
+	GetServerSidePropsContext,
+	GetServerSideProps,
+	InferGetServerSidePropsType,
 } from 'next';
 import { GetCommand, GetCommandInput } from '@aws-sdk/lib-dynamodb';
 
@@ -15,55 +15,53 @@ import { useDispatch } from '../../../lib/redux/store';
 import { addToCart } from '../../../lib/redux/slices/cartSlice';
 
 export default ({
-  product,
+	product,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  const dispath = useDispatch();
+	const dispath = useDispatch();
 
-  return (
-    <Layout>
-      <div className='p-1rem'>
-        <div className='mb-3rem text-2xl uppercase'>
-          <h2 className='font-bold'>{product.name}</h2>
-        </div>
-        <div className='flex flex-col md:flex-row '>
-          <div className='relative w-full h-full aspect-[168/227]'>
-            <Image
-              alt='description'
-              src={product.imgUrl}
-              fill
-              className='absolute flex -z-10 bg-[rgba(0,0,0,0.03)]'
-            />
-          </div>
-          <div className='pt-2 flex flex-row justify-between'>
-            <div className='flex flex-col text-xs'>
-              <span className='uppercase'>{product.name}</span>
-              <span>${product.price}.00</span>
-              <button
-                onClick={() => dispath(addToCart(product))}
-                className='bg-b text-white rounded-xs w-full'
-              >
-                Add to cart
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </Layout>
-  );
+	return (
+		<Layout>
+			<div className='p-1rem'>
+				<div className='mb-3rem text-2xl uppercase'>
+					<h2 className='font-bold'>{product.name}</h2>
+				</div>
+				<div className='flex flex-col md:flex-row '>
+					<div className='relative w-full h-full aspect-[168/227]'>
+						<Image
+							alt='description'
+							src={product.imgUrl}
+							fill
+							className='absolute flex -z-10 bg-[rgba(0,0,0,0.03)]'
+						/>
+					</div>
+					<div className='pt-2 flex flex-row justify-between'>
+						<div className='flex flex-col text-xs'>
+							<span className='uppercase'>{product.name}</span>
+							<span>${product.price}.00</span>
+							<button
+								onClick={() => dispath(addToCart(product))}
+								className='bg-b text-white rounded-xs w-full'>
+								Add to cart
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</Layout>
+	);
 };
 
 export const getServerSideProps: GetServerSideProps = async (
-  context: GetServerSidePropsContext
+	context: GetServerSidePropsContext,
 ) => {
-  const pk = context.query?.pk;
+	const pk = context.query?.pk;
 
-  const params: GetCommandInput = {
-    TableName: 'ecomm',
-    Key: { pk: `${pk}`, sk: 'METADATA' },
-  };
+	const params: GetCommandInput = {
+		TableName: 'ecomm',
+		Key: { pk: `${pk}`, sk: 'METADATA' },
+	};
 
-  const item = await client.send(new GetCommand(params));
-  console.log(item.Item);
+	const item = await client.send(new GetCommand(params));
 
-  return { props: { product: item.Item } };
+	return { props: { product: item.Item } };
 };
