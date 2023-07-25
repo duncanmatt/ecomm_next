6189e31fcf6eec2265110f6cef123afa0df9d04b
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2023-07-25 15:44:13 -0400

    plus/minus with pseudoelements

--- src/components/cart/QuantityBar.tsx
+++ src/components/cart/QuantityBar.tsx
@@ -1,43 +1,42 @@
 import { useDispatch } from '../../../lib/redux/store';
 import {
-	removeItem,
-	incrementQty,
-	decrementQty,
+  removeItem,
+  incrementQty,
+  decrementQty,
 } from '../../../lib/redux/slices/cartSlice';
 import TrashCanIcon from '../icons/TrashCanIcon';
 
 type Qty = {
-	pk: string;
-	qty: number;
+  pk: string;
+  qty: number;
 };
 
 const QuantityBar = ({ pk, qty }: Qty) => {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	return (
-		<div className='flex flex-row flex-nowrap items-center justify-evenly'>
-			<div>
-				<span
-					className='cursor-pointer'
-					onClick={() => dispatch(incrementQty(pk))}>
-					+
-				</span>
-				<span className='mx-1 font-bold'>{qty}</span>
-				<span
-					className='cursor-pointer'
-					onClick={() => dispatch(decrementQty(pk))}>
-					-
-				</span>
-			</div>
-			<div>
-				<span
-					className='cursor-pointer'
-					onClick={() => dispatch(removeItem(pk))}>
-					<TrashCanIcon />
-				</span>
-			</div>
-		</div>
-	);
+  return (
+    <div className='flex flex-row flex-nowrap items-center basis-60 justify-between'>
+      <div className='flex flex-row'>
+        <span
+          className='cursor-pointer before:content-["+"] before:flex before:flex-row before:items-center before:font-bold before:justify-center'
+          onClick={() => dispatch(incrementQty(pk))}
+        ></span>
+        <span className='mx-1 font-bold'>{qty}</span>
+        <span
+          className='cursor-pointer before:content-["-"] before:flex before:flex-row before:items-center font-bold before:justify-center'
+          onClick={() => dispatch(decrementQty(pk))}
+        ></span>
+      </div>
+      <div>
+        <span
+          className='cursor-pointer'
+          onClick={() => dispatch(removeItem(pk))}
+        >
+          <TrashCanIcon />
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default QuantityBar;
