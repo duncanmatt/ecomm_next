842a7cc45e3a04847f5a27bb274f7471a3a9e655
duncanmatt <117763103+duncanmatt@users.noreply.github.com>
2023-07-26 14:12:34 -0400

    new quantity bar

--- src/components/cart/QuantityBar.tsx
+++ src/components/cart/QuantityBar.tsx
@@ -1,42 +1,43 @@
 import { useDispatch } from '../../../lib/redux/store';
 import {
-  removeItem,
-  incrementQty,
-  decrementQty,
+	removeItem,
+	incrementQty,
+	decrementQty,
 } from '../../../lib/redux/slices/cartSlice';
 import TrashCanIcon from '../icons/TrashCanIcon';
 
 type Qty = {
-  pk: string;
-  qty: number;
+	pk: string;
+	qty: number;
 };
 
 const QuantityBar = ({ pk, qty }: Qty) => {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  return (
-    <div className='flex flex-row flex-nowrap items-center basis-60 justify-between'>
-      <div className='flex flex-row'>
-        <span
-          className='cursor-pointer before:content-["+"] before:flex before:flex-row before:items-center before:font-bold before:justify-center'
-          onClick={() => dispatch(incrementQty(pk))}
-        ></span>
-        <span className='mx-1 font-bold'>{qty}</span>
-        <span
-          className='cursor-pointer before:content-["-"] before:flex before:flex-row before:items-center font-bold before:justify-center'
-          onClick={() => dispatch(decrementQty(pk))}
-        ></span>
-      </div>
-      <div>
-        <span
-          className='cursor-pointer'
-          onClick={() => dispatch(removeItem(pk))}
-        >
-          <TrashCanIcon />
-        </span>
-      </div>
-    </div>
-  );
+	return (
+		<div className='flex flex-row flex-nowrap items-center flex-1 justify-between'>
+			<div className='flex items-center border-1 border-b'>
+				<button
+					className='z-1 btn--minus relative w-[1.875rem] h-[1.875rem] rounded-circle'
+					onClick={() => dispatch(decrementQty(pk))}>
+					<span className='minus'></span>
+				</button>
+				<div className='h-[1.875rem] flex items-center border-x-1 border-b'>
+					<span className='mx-1 font-bold'>{qty}</span>
+				</div>
+				<button
+					className='z-1 btn--plus relative w-[1.875rem] h-[1.875rem] rounded-circle'
+					onClick={() => dispatch(incrementQty(pk))}>
+					<span className='plus'></span>
+				</button>
+			</div>
+			<span
+				className='cursor-pointer'
+				onClick={() => dispatch(removeItem(pk))}>
+				<TrashCanIcon />
+			</span>
+		</div>
+	);
 };
 
 export default QuantityBar;
