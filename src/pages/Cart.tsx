816f730700c6127326dfdbbd6f7674a1b6dacf9c
duncanmatt <117763103+duncanmatt@users.noreply.github.com>
2023-07-26 14:24:18 -0400

    empty cart

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -18,57 +18,84 @@ const Cart = () => {
 	return (
 		<Layout>
 			<div className='p-2rem'>
-				<h2 className='font-bold text-xl text-center'>Cart ({count})</h2>
-				<div className='grid grid-cols-1 grid-rows-cart md:grid-cols-2 md:grid-rows-1'>
-					<div className='flex py-1rem '>
-						<ul className='p-1rem '>
-							{items.map((item: CartItem, index: number) => (
-								<li
-									key={index}
-									className='mb-10'>
-									<div className='grid grid-row-1 gap-x-5 grid-cols-2'>
-										<div className='relative h-full w-full '>
-											<Image
-												alt={item.name}
-												src={item.imgUrl}
-												width={168}
-												height={227}
-											/>
-										</div>
-										<div className='flex flex-col justify-around'>
-											<div>
-												<span className='flex flex-wrap'>
-													{item.name} - ${item.price}
-												</span>
-											</div>
-											<QuantityBar
-												pk={item.pk}
-												qty={item.qty}
-											/>
-										</div>
-									</div>
-								</li>
-							))}
-						</ul>
-					</div>
-					<div className='md:self-end bg-[#f2f2f2] p-1rem rounded-sm h-min'>
-						<div className='flex flex-row justify-between my-3'>
-							<span className='font-medium'>Shipping</span>
-							<span className='font-medium'>FREE</span>
-						</div>
-						<div className='flex flex-row justify-between my-3'>
-							<span className='font-medium'>Total</span>
-							<span className='font-semibold text-xl'>${price}</span>
-						</div>
-						<div className='flex'>
+				{items.length === 0 ? (
+					<>
+						<h3 className='uppercase text-center font-semibold'>
+							your shopping cart is empty
+						</h3>
+						<div className='flex flex-col mt-2rem gap-2'>
 							<Link
-								className='flex flex-1 text-center justify-center border-2 border-transparent h-[2.125rem] items-center font-semibold bg-b rounded-xs text-white'
+								className='bg-b border-2 border-transparent h-[2.4rem] flex items-center justify-center rounded-xs text-white'
+								href='/Login'>
+								Login
+							</Link>
+							<Link
+								className='bg-b border-2 border-transparent h-[2.4rem] flex items-center justify-center rounded-xs text-white'
 								href='/Checkout'>
-								Checkout
+								Checkout as guest
+							</Link>
+							<Link
+								className='bg-b border-2 border-transparent h-[2.4rem] flex items-center justify-center rounded-xs text-center text-white'
+								href='/'>
+								Continue shopping
 							</Link>
 						</div>
-					</div>
-				</div>
+					</>
+				) : (
+					<>
+						<h2 className='font-bold text-xl text-center'>Cart ({count})</h2>
+						<div className='grid grid-cols-1 grid-rows-cart md:grid-cols-2 md:grid-rows-1'>
+							<div className='flex py-1rem '>
+								<ul className='p-1rem '>
+									{items.map((item: CartItem, index: number) => (
+										<li
+											key={index}
+											className='mb-10'>
+											<div className='grid grid-row-1 gap-x-5 grid-cols-2'>
+												<div className='relative h-full w-full '>
+													<Image
+														alt={item.name}
+														src={item.imgUrl}
+														width={168}
+														height={227}
+													/>
+												</div>
+												<div className='flex flex-col justify-around'>
+													<div>
+														<span className='flex flex-wrap'>
+															{item.name} - ${item.price}
+														</span>
+													</div>
+													<QuantityBar
+														pk={item.pk}
+														qty={item.qty}
+													/>
+												</div>
+											</div>
+										</li>
+									))}
+								</ul>
+							</div>
+							<div className='md:self-end bg-[#f2f2f2] p-1rem rounded-sm h-min'>
+								<div className='flex flex-row justify-between my-3'>
+									<span className='font-medium'>Shipping</span>
+									<span className='font-medium'>FREE</span>
+								</div>
+								<div className='flex flex-row justify-between my-3'>
+									<span className='font-medium'>Total</span>
+									<span className='font-semibold text-xl'>${price}</span>
+								</div>
+								<div className='flex'>
+									<Link
+										className='flex flex-1 text-center justify-center border-2 border-transparent h-[2.125rem] items-center font-semibold bg-b rounded-xs text-white'
+										href='/Checkout'>
+										Checkout
+									</Link>
+								</div>
+							</div>
+						</div>
+					</>
+				)}
 			</div>
 		</Layout>
 	);
