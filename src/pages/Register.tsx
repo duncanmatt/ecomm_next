3bc261bcde5009bb658d482587a307f8ad2f605e
duncanmatt <117763103+duncanmatt@users.noreply.github.com>
2023-07-29 13:41:04 -0400

    Update Register.tsx

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,63 +1,133 @@
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 import React from 'react';
-import { signIn } from 'next-auth/react';
+import Layout from '@/components/Layout';
 
 const Register = () => {
-	const router = useRouter();
 	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 		const formData = new FormData(e.currentTarget);
 		const email = formData.get('email');
 		const password = formData.get('password');
 		const password2 = formData.get('password2');
+		const firstName = formData.get('firstName');
+		const lastName = formData.get('lastName');
+
+		const userData = { firstName, lastName, email, password };
 
 		if (password2 !== password) {
 			// toast message
 			throw new Error("passwords don't match");
 		}
+
+		const response = await fetch('http://localhost:3000/api/auth/register', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(userData),
+		});
+
+		const profile = await response.json();
+		console.log(profile);
 	};
 
 	return (
-		<div>
-			<h1>Create an account</h1>
-			<form
-				method='post'
-				onSubmit={handleSubmit}
-				action='/api/auth/register'>
-				<label htmlFor='email'>email</label>
-				<br />
-				<input
-					type='email'
-					id='email'
-					name='email'
-				/>
-				<br />
-				<label htmlFor='password'>password</label>
-				<br />
-				<input
-					type='password'
-					id='password'
-					name='password'
-				/>
-				<br />
-				<label htmlFor='password2'>confirm password</label>
-				<br />
-				<input
-					type='password'
-					id='password2'
-					name='password2'
-				/>
-				<br />
-				<input
-					type='submit'
-					value='Create'
-				/>
-			</form>
-			<div>
-				Already have an account? <Link href='/Login'>Login</Link>
+		<Layout>
+			<div className='min-h-main relative'>
+				<div className='p-3rem'>
+					<div className='flex flex-col justify-center'>
+						<div className='flex bg-[#f2f2f2] rounded-sm'>
+							<form
+								className='p-4 justify-between flex flex-1 gap-3 flex-col '
+								onSubmit={handleSubmit}>
+								<div className='flex flex-col flex-1 gap-2'>
+									<span className='flex flex-row gap-2 items-center h-8'>
+										<label
+											className=' text-center font-semibold basis-30'
+											htmlFor='firstName'>
+											First Name
+										</label>
+										<input
+											className='rounded-xl border-2 bg-inherit border-b rounded-xs flex flex-1 h-full flex-0 px-4'
+											type='text'
+											id='firstName'
+											name='firstName'
+										/>
+									</span>
+									<span className='flex flex-row gap-2 items-center h-8'>
+										<label
+											className='text-center font-semibold basis-30'
+											htmlFor='lastName'>
+											Last Name
+										</label>
+										<input
+											className='rounded-xl border-2 bg-inherit border-b rounded-xs flex flex-1 h-full flex-0 px-4'
+											type='text'
+											id='lastName'
+											name='lastName'
+										/>
+									</span>
+									<span className='flex flex-row gap-2 items-center h-8'>
+										<label
+											className='text-center font-semibold basis-30'
+											htmlFor='email'>
+											Email
+										</label>
+										<input
+											className='rounded-xl border-2 bg-inherit border-b rounded-xs flex flex-1 h-full flex-0 px-4'
+											type='email'
+											id='email'
+											name='email'
+											required
+										/>
+									</span>
+									<span className='flex flex-row gap-2 items-center h-8'>
+										<label
+											className=' text-center font-semibold basis-30'
+											htmlFor='password'>
+											Password
+										</label>
+										<input
+											className='rounded-xl border-2 bg-inherit border-b rounded-xs flex flex-1 h-full flex-0 px-4'
+											type='password'
+											id='password'
+											name='password'
+											required
+										/>
+									</span>
+									<span className='flex flex-row gap-2 items-center h-8'>
+										<label
+											className='text-center font-semibold basis-30'
+											htmlFor='password2'>
+											Confirm Password
+										</label>
+										<input
+											className='rounded-xl border-2 bg-inherit border-b rounded-xs flex flex-1 h-full flex-0 px-4'
+											type='password'
+											id='password2'
+											name='password2'
+										/>
+									</span>
+								</div>
+								<div>
+									<span className='flex mb-2 h-10'>
+										<input
+											className='border-2 border-transparent text-white font-bold bg-b hover:bg-[#c1c1c1] hover:text-b rounded-reg flex-1'
+											type='submit'
+											value='Register'
+										/>
+									</span>
+								</div>
+							</form>
+						</div>
+						<div className='flex flex-wrap'>
+							<span className='mx-1'>Already have an account?</span>
+							<Link href='/Register'>Login</Link>
+						</div>
+					</div>
+				</div>
 			</div>
-		</div>
+		</Layout>
 	);
 };
 
