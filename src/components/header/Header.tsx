0b15b675e200a2e1b35a12ffe0dbf796a5b3a25e
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2023-08-01 14:35:41 -0400

    header bg

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,51 +4,52 @@ import Desktop from './Desktop';
 import React from 'react';
 
 const Header = () => {
-	const [scrollDir, setScrollDir] = useState('up');
+  const [scrollDir, setScrollDir] = useState('up');
 
-	useEffect(() => {
-		const threshold = 3;
-		let lastY = window.pageYOffset;
-		let ticking = false;
+  useEffect(() => {
+    const threshold = 3;
+    let lastY = window.pageYOffset;
+    let ticking = false;
 
-		const updateScrollDir = () => {
-			const y = window.pageYOffset;
+    const updateScrollDir = () => {
+      const y = window.pageYOffset;
 
-			if (Math.abs(y - lastY) < threshold) {
-				ticking = false;
-				return;
-			}
-			setScrollDir(y > lastY ? 'down' : 'up');
-			lastY = y > 0 ? y : 0;
-			ticking = false;
-		};
+      if (Math.abs(y - lastY) < threshold) {
+        ticking = false;
+        return;
+      }
+      setScrollDir(y > lastY ? 'down' : 'up');
+      lastY = y > 0 ? y : 0;
+      ticking = false;
+    };
 
-		const onScroll = () => {
-			if (!ticking) {
-				window.requestAnimationFrame(updateScrollDir);
-				ticking = true;
-			}
-		};
+    const onScroll = () => {
+      if (!ticking) {
+        window.requestAnimationFrame(updateScrollDir);
+        ticking = true;
+      }
+    };
 
-		window.addEventListener('scroll', onScroll);
+    window.addEventListener('scroll', onScroll);
 
-		return () => window.removeEventListener('scroll', onScroll);
-	}, [scrollDir]);
+    return () => window.removeEventListener('scroll', onScroll);
+  }, [scrollDir]);
 
-	return (
-		<>
-			<header
-				data-action={`${scrollDir}`}
-				className={`h-60 z-[60] bg-[#f9f9f9] fixed bottom-auto top-0 right-0 left-0`}>
-				<div className='relative bg-scroll px-1rem md:hidden h-full'>
-					<Mobile />
-				</div>
-				<div className='px-1rem hidden md:block h-full'>
-					<Desktop />
-				</div>
-			</header>
-		</>
-	);
+  return (
+    <>
+      <header
+        data-action={`${scrollDir}`}
+        className={`h-60 z-[60] backdrop-blur-6xl bg-faded fixed bottom-auto top-0 right-0 left-0`}
+      >
+        <div className='relative bg-scroll px-1rem md:hidden h-full'>
+          <Mobile />
+        </div>
+        <div className='px-1rem hidden md:block h-full'>
+          <Desktop />
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default Header;
