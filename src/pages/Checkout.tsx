29d848c1d2892347c042236cd2cf88b28fa38547
duncanmatt <117763103+duncanmatt@users.noreply.github.com>
2023-08-09 00:04:42 -0400

    remove redirect from checkout page

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -5,72 +5,74 @@ import Layout from '@/components/Layout';
 import CheckoutForm from '@/components/cart/CheckoutForm';
 import type { StripeElementsOptions } from '@stripe/stripe-js';
 import {
-  cartItems,
-  cartTotal,
+	cartItems,
+	cartTotal,
 } from '../../lib/redux/slices/cartSlice/selectors';
 import { userProfile } from '../../lib/redux/slices/authSlice/selectors';
 import { useSelector } from '../../lib/redux/store';
 import { getServerSession } from 'next-auth/next';
-import { authOptions } from './api/auth/[...nextauth]';
+// import { authOptions } from './api/auth/[...nextauth]';
 import type { GetServerSidePropsContext } from 'next';
 
 const s = getStripe();
 
 const Checkout = () => {
-  const items = useSelector(cartItems);
-  const total = useSelector(cartTotal);
-  const user = useSelector(userProfile);
-  const [clientSecret, setClientSecret] = useState('');
+	const items = useSelector(cartItems);
+	const total = useSelector(cartTotal);
+	const user = useSelector(userProfile);
+	const [clientSecret, setClientSecret] = useState('');
 
-  useEffect(() => {
-    fetch(
-      'https://c4z5zswbfk.execute-api.us-east-1.amazonaws.com/api/stripe/create-payment-intent',
-      {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ items }),
-      }
-    )
-      .then((res) => res.json())
-      .then((data) => setClientSecret(data.clientSecret));
-  }, []);
+	useEffect(() => {
+		fetch(
+			'https://c4z5zswbfk.execute-api.us-east-1.amazonaws.com/api/stripe/create-payment-intent',
+			{
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({ items }),
+			},
+		)
+			.then(res => res.json())
+			.then(data => setClientSecret(data.clientSecret));
+	}, []);
 
-  const appearance = {
-    theme: 'stripe' as const,
-  };
-  const options: StripeElementsOptions = {
-    clientSecret,
-    appearance,
-  };
+	const appearance = {
+		theme: 'stripe' as const,
+	};
+	const options: StripeElementsOptions = {
+		clientSecret,
+		appearance,
+	};
 
-  return (
-    <Layout>
-      {clientSecret && (
-        <div className='p-1rem'>
-          <h2 className='font-bold text-center'>${total}.00</h2>
-          <Elements options={options} stripe={s}>
-            <CheckoutForm />
-          </Elements>
-        </div>
-      )}
-    </Layout>
-  );
+	return (
+		<Layout>
+			{clientSecret && (
+				<div className='p-1rem'>
+					<h2 className='font-bold text-center'>${total}.00</h2>
+					<Elements
+						options={options}
+						stripe={s}>
+						<CheckoutForm />
+					</Elements>
+				</div>
+			)}
+		</Layout>
+	);
 };
 
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const session = await getServerSession(context.req, context.res, authOptions);
+// export async function getServerSideProps(context: GetServerSidePropsContext) {
+//   const session = await getServerSession(context.req, context.res, authOptions);
 
-  if (session) {
-    return { props: { user: { email: session.user?.email } } };
-  }
+//   if (session) {
+//     return { props: { user: { email: session.user?.email } } };
+//   }
 
-  return {
-    redirect: {
-      permanent: false,
-      destination: '/Login',
-    },
-    props: {},
-  };
-}
+//   return {
+//     redirect: {
+//       permanent: false,
+//       destination: '/Login',
+//     },
+//     props: {},
+//   };
+// }
 
 export default Checkout;
