3d3d544e60b99af6853615309671be2712a4fa36
duncanmatt <117763103+duncanmatt@users.noreply.github.com>
2023-08-09 00:11:51 -0400

    remove server side check from profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,50 +1,50 @@
 import Link from 'next/link';
 import Layout from '@/components/Layout';
 import { signOut, useSession } from 'next-auth/react';
-import { getServerSession } from 'next-auth/next';
-import { authOptions } from './api/auth/[...nextauth]';
-import { GetServerSidePropsContext } from 'next';
+// import { getServerSession } from 'next-auth/next';
+// import { authOptions } from './api/auth/[...nextauth]';
+// import { GetServerSidePropsContext } from 'next';
 
 const Profile = () => {
-  const { data: session } = useSession();
+	const { data: session } = useSession();
 
-  if (session) {
-    return (
-      <>
-        <Layout>
-          <div className='py-3 flex flex-col items-center justify-center'>
-            <div>Signed in as {session.user?.email}</div>
-            <br />
-            <button onClick={() => signOut()}>Sign out</button>
-          </div>
-        </Layout>
-      </>
-    );
-  }
-  return (
-    <>
-      Not signed in <br />
-      <Link href='/Login'>Login</Link>
-    </>
-  );
+	if (session) {
+		return (
+			<>
+				<Layout>
+					<div className='py-3 flex flex-col items-center justify-center'>
+						<div>Signed in as {session.user?.email}</div>
+						<br />
+						<button onClick={() => signOut()}>Sign out</button>
+					</div>
+				</Layout>
+			</>
+		);
+	}
+	return (
+		<>
+			Not signed in <br />
+			<Link href='/Login'>Login</Link>
+		</>
+	);
 };
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
-  const session = await getServerSession(context.req, context.res, authOptions);
+// export const getServerSideProps = async (
+//   context: GetServerSidePropsContext
+// ) => {
+//   const session = await getServerSession(context.req, context.res, authOptions);
 
-  if (session) {
-    return { props: { user: { email: session.user?.email } } };
-  }
+//   if (session) {
+//     return { props: { user: { email: session.user?.email } } };
+//   }
 
-  return {
-    redirect: {
-      destination: '/Login',
-      permanent: false,
-    },
-    props: {},
-  };
-};
+//   return {
+//     redirect: {
+//       destination: '/Login',
+//       permanent: false,
+//     },
+//     props: {},
+//   };
+// };
 
 export default Profile;
