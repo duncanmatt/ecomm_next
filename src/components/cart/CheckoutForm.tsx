3e73777bfb191bc532244f7d622553403ebd66e5
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2023-08-10 22:23:22 -0400

    cart colors

--- src/components/cart/CheckoutForm.tsx
+++ src/components/cart/CheckoutForm.tsx
@@ -87,22 +87,28 @@ const CheckoutForm = () => {
   };
 
   return (
-    <form id='payment-form' onSubmit={handleSubmit}>
-      <LinkAuthenticationElement
-        id='link-authentication-element'
-        onChange={(e: StripeLinkAuthenticationElementChangeEvent) =>
-          setEmail(e.value.email)
-        }
-      />
-      <PaymentElement id='payment-element' options={paymentElementOptions} />
-      <button disabled={isLoading || !stripe || !elements} id='submit'>
-        <span id='button-text'>
-          {isLoading ? <div className='spinner' id='spinner'></div> : 'Pay now'}
-        </span>
-      </button>
-      {/* Show any error or success messages */}
-      {message && <div id='payment-message'>{message}</div>}
-    </form>
+    <div className='mt-[60px]'>
+      <form id='payment-form' onSubmit={handleSubmit}>
+        <LinkAuthenticationElement
+          id='link-authentication-element'
+          onChange={(e: StripeLinkAuthenticationElementChangeEvent) =>
+            setEmail(e.value.email)
+          }
+        />
+        <PaymentElement id='payment-element' options={paymentElementOptions} />
+        <button disabled={isLoading || !stripe || !elements} id='submit'>
+          <span id='button-text'>
+            {isLoading ? (
+              <div className='spinner' id='spinner'></div>
+            ) : (
+              'Confirm Checkout'
+            )}
+          </span>
+        </button>
+        {/* Show any error or success messages */}
+        {message && <div id='payment-message'>{message}</div>}
+      </form>
+    </div>
   );
 };
 
