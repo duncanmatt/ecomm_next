850259a17a41af64ba07de3c57d5554efca6c3ca
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2023-08-11 13:52:28 -0400

    update product card

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -10,7 +10,7 @@ const ProductCard = (product: Product) => {
   const dispatch = useDispatch();
 
   return (
-    <li className='mb-16 h-[inherit] bg-65 rounded-md relative'>
+    <li className='mb-16 h-[inherit] bg-75 rounded-md relative'>
       <span className='bg-white rounded-circle absolute z-10 top-3 right-4'>
         <button
           className='relative -translate-y-[1px] flex items-center justify-center h-[1.875rem] w-[1.875rem] z-5'
@@ -27,7 +27,7 @@ const ProductCard = (product: Product) => {
           query: { productId: product.productId },
         }}
       >
-        <div className='relative aspect-[0.85/1]'>
+        <div className='relative bg-gradi aspect-[0.9/1]'>
           <Image
             alt='description'
             src={product.imgUrl}
@@ -37,19 +37,23 @@ const ProductCard = (product: Product) => {
             className='flex'
           />
         </div>
-        <div className='px-3 pb-3 z-[4]'>
-          <div className='flex flex-col w-full'>
-            <div className='flex flex-wrap justify-between items-center w-full'>
-              <span className='text-base uppercase tracking-wide font-semibold'>
-                <h5>{product.name}</h5>
-              </span>
-              <span>
-                {formatAmountForDisplay(Number(product.price), 'usd')}
-              </span>
+        <div className='px-3 pb-2 z-[4]'>
+          <div className='flex w-full'>
+            <div className='flex flex-col w-full'>
+              <div className=''>
+                <h5 className='text-lg font-medium tracking-wide'>
+                  {product.name}
+                </h5>
+              </div>
+              <div className='flex flex-row justify-between items-center w-full'>
+                <span className='font-light text-base'>
+                  {formatAmountForDisplay(Number(product.price), 'usd')}
+                </span>
+                <span className='font text-xs uppercase tracking-tight'>
+                  sold out
+                </span>
+              </div>
             </div>
-            <span className='font-medium text-xs uppercase tracking-tight'>
-              sold out
-            </span>
           </div>
         </div>
       </Link>
