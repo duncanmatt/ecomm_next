d514a456e9f4edf6325a542173ae6e1233a50f00
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2023-08-10 23:43:38 -0400

    product card component

---
+++ src/components/products/ProductCard.tsx
@@ -0,0 +1,60 @@
+import { useDispatch } from '../../../lib/redux/store';
+import { addToCart } from '../../../lib/redux/slices/cartSlice';
+import { formatAmountForDisplay } from '../../../utils/stripe-helpers';
+import { Product } from '../../../interfaces';
+import Image from 'next/image';
+import Link from 'next/link';
+import CartIcon from '@/components/icons/CartIcon';
+
+const ProductCard = (product: Product) => {
+  const dispatch = useDispatch();
+
+  return (
+    <li className='mb-16 h-[inherit] bg-65 rounded-md relative'>
+      <span className='bg-white rounded-circle absolute z-10 top-3 right-4'>
+        <button
+          className='relative -translate-y-[1px] flex items-center justify-center h-[1.875rem] w-[1.875rem] z-5'
+          onClick={() => dispatch(addToCart(product))}
+        >
+          <CartIcon />
+        </button>
+      </span>
+      <Link
+        className='z-0 h-full w-full'
+        // as='/products/product'
+        href={{
+          pathname: `/products/product`,
+          query: { productId: product.productId },
+        }}
+      >
+        <div className='relative aspect-[0.85/1]'>
+          <Image
+            alt='description'
+            src={product.imgUrl}
+            placeholder='blur'
+            blurDataURL='ttps://ecomm-imgs-test.s3.amazonaws.com/ecomm-imgs-test/tile.jpg'
+            fill
+            className='flex'
+          />
+        </div>
+        <div className='px-3 pb-3 z-[4]'>
+          <div className='flex flex-col w-full'>
+            <div className='flex flex-wrap justify-between items-center w-full'>
+              <span className='text-base uppercase tracking-wide font-semibold'>
+                <h5>{product.name}</h5>
+              </span>
+              <span>
+                {formatAmountForDisplay(Number(product.price), 'usd')}
+              </span>
+            </div>
+            <span className='font-medium text-xs uppercase tracking-tight'>
+              sold out
+            </span>
+          </div>
+        </div>
+      </Link>
+    </li>
+  );
+};
+
+export default ProductCard;
--- src/pages/products/collection/index.tsx
+++ src/pages/products/collection/index.tsx
@@ -1,23 +1,16 @@
-import { cartSlice } from '../../../../lib/redux/slices/cartSlice/cartSlice';
 import {
   GetServerSideProps,
   GetServerSidePropsContext,
   InferGetServerSidePropsType,
 } from 'next';
-import { useDispatch } from '../../../../lib/redux/store';
-import { fetchGetJSON } from '../../../../utils/api-helpers';
-import { formatAmountForDisplay } from '../../../../utils/stripe-helpers';
-import { Product } from '../../../../interfaces';
-import Image from 'next/image';
 import Layout from '@/components/Layout';
-import Link from 'next/link';
-import CartIcon from '@/components/icons/CartIcon';
+import { fetchGetJSON } from '../../../../utils/api-helpers';
+import { Product } from '../../../../interfaces';
+import ProductCard from '@/components/products/ProductCard';
 
 export default ({
   products,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  const dispatch = useDispatch();
-
   return (
     <Layout>
       <div className='p-1rem'>
@@ -26,53 +19,7 @@ export default ({
         </div>
         <ul className='grid grid-rows-1 grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-x-4'>
           {products?.map((product: Product, index: number) => (
-            <li
-              className='mb-16 h-[inherit] bg-60 rounded-md relative'
-              key={index}
-            >
-              <span className='bg-white rounded-circle absolute z-10 top-3 right-4'>
-                <button
-                  className='relative -translate-y-[1px] flex items-center justify-center h-[1.875rem] w-[1.875rem] z-5'
-                  onClick={() => dispatch(cartSlice.actions.addToCart(product))}
-                >
-                  <CartIcon />
-                </button>
-              </span>
-              <Link
-                className='z-0 h-full w-full'
-                // as='/products/product'
-                href={{
-                  pathname: `/products/product`,
-                  query: { productId: product.productId },
-                }}
-              >
-                <div className='relative aspect-[0.85/1]'>
-                  <Image
-                    alt='description'
-                    src={product.imgUrl}
-                    placeholder='blur'
-                    blurDataURL='ttps://ecomm-imgs-test.s3.amazonaws.com/ecomm-imgs-test/tile.jpg'
-                    fill
-                    className='flex'
-                  />
-                </div>
-                <div className='px-3 pb-3 z-[4]'>
-                  <div className='flex flex-col w-full'>
-                    <div className='flex flex-wrap justify-between items-center w-full'>
-                      <span className='text-base uppercase tracking-wide font-semibold'>
-                        <h5>{product.name}</h5>
-                      </span>
-                      <span>
-                        {formatAmountForDisplay(Number(product.price), 'usd')}
-                      </span>
-                    </div>
-                    <span className='font-medium text-xs uppercase tracking-tight'>
-                      sold out
-                    </span>
-                  </div>
-                </div>
-              </Link>
-            </li>
+            <ProductCard key={index} {...product} />
           ))}
         </ul>
       </div>
