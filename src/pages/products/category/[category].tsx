1adaece6e46bdd5632ad0634b71ec4e44e98f320
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2023-08-11 23:16:48 -0400

    prepare for product filtering

--- src/pages/products/category/[category].tsx
+++ src/pages/products/category/[category].tsx
@@ -6,16 +6,26 @@ import type {
 } from 'next';
 import { Product } from '../../../../interfaces';
 import ProductCard from '@/components/products/ProductCard';
+import SearchIcon from '@/components/icons/SearchIcon';
 
 export default ({
   products,
   category,
+  quantity,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   return (
     <Layout>
-      <div className='px-1rem py-3rem'>
-        <div className='mt-8 mb-2rem uppercase text-center'>
-          <h2 className='text-2xl tracking-widest'>{category}</h2>
+      <div className='px-1rem mt-[7.45rem]'>
+        <div className='uppercase text-center'>
+          <h2 className='font-10 font-semibold text-lg tracking-widest'>
+            {category}
+          </h2>
+        </div>
+        <div className='border-b-1 border-5 px-2 my-3rem'>
+          <div className='w-full text-sm h-[2.125rem] flex flex-row justify-between items-center'>
+            <span>({quantity}) selections</span>
+            <button>filter & sort</button>
+          </div>
         </div>
         <ul className='grid grid-rows-1 grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-x-4'>
           {products?.map((product: Product, index: number) => (
@@ -42,5 +52,5 @@ export const getServerSideProps: GetServerSideProps = async (
 
   const data = await response.json();
 
-  return { props: { products: data.Items, category } };
+  return { props: { products: data.Items, quantity: data.Count, category } };
 };
--- src/pages/products/collection/index.tsx
+++ src/pages/products/collection/index.tsx
@@ -10,12 +10,21 @@ import ProductCard from '@/components/products/ProductCard';
 
 export default ({
   products,
+  quantity,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   return (
     <Layout>
-      <div className='px-1rem py-3rem'>
-        <div className='mt-8 mb-2rem text-center uppercase'>
-          <h2 className='font-medium text-2xl'>summer 2023</h2>
+      <div className='px-1rem mt-[7.45rem]'>
+        <div className='text-center uppercase'>
+          <h2 className='font-semibold font-10 text-lg tracking-widest'>
+            summer 2023
+          </h2>
+        </div>
+        <div className='border-b-1 border-5 px-2 my-3rem'>
+          <div className='w-full text-sm h-[2.125rem] flex flex-row justify-between items-center'>
+            <span>{quantity} selections</span>
+            <button className='capitalize'>filter & sort</button>
+          </div>
         </div>
         <ul className='grid grid-rows-1 grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-x-4'>
           {products?.map((product: Product, index: number) => (
@@ -35,6 +44,7 @@ export const getServerSideProps: GetServerSideProps = async (
   );
 
   const products: Product[] = response.Items;
+  const quantity: number = response.Count;
 
-  return { props: { products } };
+  return { props: { products, quantity } };
 };
