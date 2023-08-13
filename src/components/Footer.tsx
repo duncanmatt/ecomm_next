bf7e9dea3150c6a732a951feb43e303d872a5b20
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2023-08-13 00:30:46 -0400

    update footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -30,16 +30,19 @@ const mediaIcons = [
 
 export default () => {
   return (
-    <div className='px-1rem py-2rem'>
+    <div className=''>
       <div className='flex flex-col'>
-        <div className='flex flex-wrap w-full justify-between'>
-          <h4 className='text-lg font-semibold tracking-wide text-center '>
+        <div className='flex flex-wrap max-sm:py-5 w-full max-sm:text-center h-[6rem] border-y-1 border-80 max-sm:justify-around justify-between'>
+          <h6 className='max-sm:w-full font-semibold tracking-normal'>
             the black sheep
-          </h4>
-          <p>fashionably comfortable streetwear</p>
+          </h6>
+          <p className='max-sm:w-full text-xs'>
+            fashionably comfortable streetwear
+          </p>
         </div>
-        <div>
-          <ul className='flex flex-wrap justify-center gap-1 pt-1rem'>
+        <div className='flex py-2 flex-wrap items-center justify-between w-full'>
+          <span className='font-light text-sm px-1'>Follow us:</span>
+          <ul className='flex flex-row gap-1'>
             {mediaIcons.map((icon, index) => (
               <li className='bg-[#f2f2f2] rounded-circle' key={index}>
                 <span className='flex items-center justify-center shrink h-[1.875rem] w-[1.875rem]'>
@@ -48,6 +51,9 @@ export default () => {
               </li>
             ))}
           </ul>
+        </div>
+        <div className='text-center py-1rem font-light text-xs border-t-1 border-80'>
+          2023 Black Sheep World
         </div>
       </div>
     </div>
