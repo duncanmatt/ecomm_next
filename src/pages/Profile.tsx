17774f539730fadd8623dab9f1188a16e2e74c88
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2023-08-15 20:38:09 -0400

    fix padding on profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -16,7 +16,7 @@ const Profile = () => {
       <>
         <Layout>
           <div className='mt-[160px]'>
-            <div className='py-3rem px:1rem md:px-3rem w-full flex flex-col items-center justify-center'>
+            <div className='py-3rem px-1rem md:px-3rem w-full flex flex-col items-center justify-center'>
               <div className='text-xl'>
                 Welcome back,{' '}
                 <span className='font-semibold'>
@@ -24,7 +24,7 @@ const Profile = () => {
                 </span>
               </div>
               <div className='py-1rem'>Email: {session.user?.email}</div>
-              <div className='py-1rem flex'>
+              <div className='py-1rem '>
                 <button
                   className='border-2 border-transparent text-bold flex-1 md rounded-reg text-white bg-5 hover:bg-50 hover:text-5'
                   onClick={() => signOut()}
