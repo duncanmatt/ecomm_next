ce42b14a2554418b1ae55eb32bf4d4db5f149457
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2023-08-17 12:23:05 -0400

    header opacity

--- src/components/header/Menu.tsx
+++ src/components/header/Menu.tsx
@@ -18,7 +18,7 @@ const links = [
 
 const Menu = ({ fn }: any) => {
   return (
-    <div className='h-[calc(100vh-60px)] bg-faded w-full p-1rem'>
+    <div className='h-[calc(100vh-60px)] bg-faded backdrop-blur-xl w-full p-1rem'>
       <ul className='block relative transition-all'>
         {links.map((link) => (
           <li key={link.id} className='pb-1rem animate-fadeIn'>
