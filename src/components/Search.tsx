31681d3441ecb03e27af159979d200edef3bea72
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2023-08-17 17:30:35 -0400

    absolute search url

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -12,7 +12,7 @@ const Search = ({ fn }: any) => {
   useEffect(() => {
     const getProducts = async () => {
       const { Items } = await fetchGetJSON(
-        (process.env.API_URL + '/api/products') as string
+        'https://c4z5zswbfk.execute-api.us-east-1.amazonaws.com/api/products'
       );
       setProducts(Items);
     };
