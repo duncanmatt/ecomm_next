ab692bf51a0da55c042541090339a5797ab156e2
duncanmatt <117763103+duncanmatt@users.noreply.github.com>
2023-08-18 08:23:22 -0400

    shipping info on checkout form

--- src/components/cart/CheckoutForm.tsx
+++ src/components/cart/CheckoutForm.tsx
@@ -1,120 +1,155 @@
 import { useState, useEffect } from 'react';
 import {
-  PaymentElement,
-  LinkAuthenticationElement,
-  useStripe,
-  useElements,
+	PaymentElement,
+	LinkAuthenticationElement,
+	useStripe,
+	useElements,
+	AddressElement,
 } from '@stripe/react-stripe-js';
 import type {
-  StripePaymentElementOptions,
-  StripeLinkAuthenticationElementChangeEvent,
+	StripePaymentElementOptions,
+	Address,
+	StripeAddressElementOptions,
+	StripeAddressElementChangeEvent,
+	StripeLinkAuthenticationElementChangeEvent,
 } from '@stripe/stripe-js';
+import { split } from 'postcss/lib/list';
 
 const CheckoutForm = () => {
-  const stripe = useStripe();
-  const elements = useElements();
+	const stripe = useStripe();
+	const elements = useElements();
 
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
+	const [email, setEmail] = useState('');
+	const [orderInfo, setOrderInfo] = useState({
+		name: '',
+		address: { line1: '', city: '', state: '', postal_code: '', country: '' },
+	});
+	const [message, setMessage] = useState('');
+	const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    if (!stripe) {
-      return;
-    }
+	useEffect(() => {
+		if (!stripe) {
+			return;
+		}
 
-    const clientSecret = new URLSearchParams(window.location.search).get(
-      'payment_intent_client_secret'
-    );
+		const clientSecret = new URLSearchParams(window.location.search).get(
+			'payment_intent_client_secret',
+		);
 
-    if (!clientSecret) {
-      return;
-    }
+		if (!clientSecret) {
+			return;
+		}
 
-    stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
-      switch (paymentIntent?.status) {
-        case 'succeeded':
-          setMessage('Payment succeeded!');
-          break;
-        case 'processing':
-          setMessage('Your payment is processing.');
-          break;
-        case 'requires_payment_method':
-          setMessage('Your payment was not successful, please try again.');
-          break;
-        default:
-          setMessage('Something went wrong.');
-          break;
-      }
-    });
-  }, [stripe]);
+		stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
+			switch (paymentIntent?.status) {
+				case 'succeeded':
+					setMessage('Payment succeeded!');
+					break;
+				case 'processing':
+					setMessage('Your payment is processing.');
+					break;
+				case 'requires_payment_method':
+					setMessage('Your payment was not successful, please try again.');
+					break;
+				default:
+					setMessage('Something went wrong.');
+					break;
+			}
+		});
+	}, [stripe]);
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
 
-    if (!stripe || !elements) {
-      // Stripe.js hasn't yet loaded.
-      // Make sure to disable form submission until Stripe.js has loaded.
-      return;
-    }
+		if (!stripe || !elements) {
+			// Stripe.js hasn't yet loaded.
+			// Make sure to disable form submission until Stripe.js has loaded.
+			return;
+		}
 
-    setIsLoading(true);
+		setIsLoading(true);
 
-    const { error } = await stripe.confirmPayment({
-      elements,
-      confirmParams: {
-        receipt_email: email,
-        return_url: 'https://dev.d2ev7e5869alah.amplifyapp.com/OrderSuccess',
-      },
-    });
+		const { error } = await stripe.confirmPayment({
+			elements,
+			confirmParams: {
+				receipt_email: email,
+				return_url: 'https://dev.d2ev7e5869alah.amplifyapp.com/OrderSuccess',
+				shipping: { address: orderInfo.address, name: orderInfo.name },
+			},
+		});
 
-    // This point will only be reached if there is an immediate error when
-    // confirming the payment. Otherwise, your customer will be redirected to
-    // your `return_url`. For some payment methods like iDEAL, your customer will
-    // be redirected to an intermediate site first to authorize the payment, then
-    // redirected to the `return_url`.
-    if (error.type === 'card_error' || error.type === 'validation_error') {
-      setMessage(error.message ? error.message : '');
-    } else {
-      setMessage('An unexpected error occurred.');
-    }
+		// This point will only be reached if there is an immediate error when
+		// confirming the payment. Otherwise, your customer will be redirected to
+		// your `return_url`. For some payment methods like iDEAL, your customer will
+		// be redirected to an intermediate site first to authorize the payment, then
+		// redirected to the `return_url`.
+		if (error.type === 'card_error' || error.type === 'validation_error') {
+			setMessage(error.message ? error.message : '');
+		} else {
+			setMessage('An unexpected error occurred.');
+		}
 
-    setIsLoading(false);
-  };
+		setIsLoading(false);
+	};
 
-  const paymentElementOptions: StripePaymentElementOptions = {
-    layout: 'tabs',
-  };
+	const paymentElementOptions: StripePaymentElementOptions = {
+		layout: 'tabs',
+	};
 
-  return (
-    <div className='mt-[60px]'>
-      <form id='payment-form' onSubmit={handleSubmit}>
-        <LinkAuthenticationElement
-          id='link-authentication-element'
-          onChange={(e: StripeLinkAuthenticationElementChangeEvent) =>
-            setEmail(e.value.email)
-          }
-        />
-        <PaymentElement id='payment-element' options={paymentElementOptions} />
-        {/* SHIPPING INFO INPUTS */}
-        <button disabled={isLoading || !stripe || !elements} id='submit'>
-          <span id='button-text'>
-            {isLoading ? (
-              <div className='spinner' id='spinner'></div>
-            ) : (
-              'Confirm Checkout'
-            )}
-          </span>
-        </button>
-        {/* Show any error or success messages */}
-        {message && (
-          <div className='text-xl text-center' id='payment-message'>
-            {message}
-          </div>
-        )}
-      </form>
-    </div>
-  );
+	const addressElementOptions: StripeAddressElementOptions = {
+		mode: 'shipping',
+		allowedCountries: ['US'],
+		display: {
+			name: 'full',
+		},
+	};
+
+	return (
+		<div className='mt-[60px]'>
+			<form
+				id='payment-form'
+				onSubmit={handleSubmit}>
+				<LinkAuthenticationElement
+					id='link-authentication-element'
+					onChange={(e: StripeLinkAuthenticationElementChangeEvent) =>
+						setEmail(e.value.email)
+					}
+				/>
+				<AddressElement
+					id='address-element'
+					options={addressElementOptions}
+					onChange={(e: StripeAddressElementChangeEvent) =>
+						setOrderInfo({ name: e.value.name, address: e.value.address })
+					}
+				/>
+				<PaymentElement
+					id='payment-element'
+					options={paymentElementOptions}
+				/>
+				<button
+					disabled={isLoading || !stripe || !elements}
+					id='submit'>
+					<span id='button-text'>
+						{isLoading ? (
+							<div
+								className='spinner'
+								id='spinner'></div>
+						) : (
+							'Confirm Checkout'
+						)}
+					</span>
+				</button>
+				{/* Show any error or success messages */}
+				{message && (
+					<div
+						className='text-xl text-center'
+						id='payment-message'>
+						{message}
+					</div>
+				)}
+			</form>
+		</div>
+	);
 };
 
 export default CheckoutForm;
