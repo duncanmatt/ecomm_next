2d983e8b5b19678ede5147f71eaaf0aff7fdd0fa
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2023-08-18 23:43:43 -0400

    adjust conditions for rendering searchon mobile

--- src/components/header/Mobile.tsx
+++ src/components/header/Mobile.tsx
@@ -30,71 +30,73 @@ const Mobile = () => {
 
   return (
     <>
-      {searchActive ? (
+      {searchActive && (
         <>
-          <Search fn={toggleSearch} />
-        </>
-      ) : (
-        <>
-          <nav className='h-full flex items-center'>
-            <div className='flex flex-row w-full justify-between items-center'>
-              <span>{menuOpen || searchActive ? <></> : <Logo />}</span>
-              <ul className='flex flex-row justify-evenly items-center'>
-                {!menuOpen && (
-                  <>
-                    <li className='py-2 inline-block'>
-                      <Link className='flex' href='/Cart'>
-                        <div className='relative'>
-                          <div className='absolute w-full h-full flex justify-center items-center font-medium text-cart'>
-                            <span className='pt-[6px]'>{cartQty}</span>
-                          </div>
-
-                          <span className='flex'>
-                            <CartIcon />
-                          </span>
-                        </div>
-                      </Link>
-                    </li>
-                    <li className='py-2 inline-block'>
-                      <span
-                        onClick={toggleSearch}
-                        className='flex mx-1rem cursor-pointer'
-                      >
-                        <SearchIcon />
-                      </span>
-                    </li>
-                  </>
-                )}
-                <li className='py-2 inline-block'>
-                  <span className='flex cursor-pointer' onClick={toggleMenu}>
-                    {menuOpen ? <CloseIcon /> : <BurgerIcon />}
-                  </span>
-                </li>
-              </ul>
-              {/* {menuOpen && (
-                <> */}
-              <div
-                id='mobileMenu'
-                className='ease-in-out duration-300'
-                style={{
-                  position: 'absolute',
-                  top: '60px',
-                  left: menuOpen ? '0' : '100%',
-                  right: '0',
-                  bottom: '0',
-                  height: '100%',
-                  width: '100%',
-                  zIndex: menuOpen ? '3' : '0',
-                }}
-              >
-                <Menu fn={toggleMenu} />
-              </div>
-              {/* </>
-              )} */}
-            </div>
-          </nav>
+          <div className='ease-in-out duration-1000 '>
+            {' '}
+            <Search fn={toggleSearch} />
+          </div>
         </>
       )}
+      <nav className='h-full flex items-center'>
+        <div className='flex flex-row w-full justify-between items-center'>
+          <span>{menuOpen || searchActive ? <></> : <Logo />}</span>
+          <ul className='flex flex-row justify-evenly items-center'>
+            {!menuOpen && (
+              <>
+                <li className='py-2 inline-block'>
+                  {!searchActive && (
+                    <Link className='flex' href='/Cart'>
+                      <div className='relative'>
+                        <div className='absolute w-full h-full flex justify-center items-center font-medium text-cart'>
+                          <span className='pt-[6px]'>{cartQty}</span>
+                        </div>
+
+                        <span className='flex'>
+                          <CartIcon />
+                        </span>
+                      </div>
+                    </Link>
+                  )}
+                </li>
+                <li className='py-2 inline-block'>
+                  <span
+                    onClick={toggleSearch}
+                    className='flex mx-1rem cursor-pointer'
+                  >
+                    <SearchIcon />
+                  </span>
+                </li>
+              </>
+            )}
+            <li className='py-2 inline-block'>
+              <span className='flex cursor-pointer' onClick={toggleMenu}>
+                {menuOpen ? <CloseIcon /> : <BurgerIcon />}
+              </span>
+            </li>
+          </ul>
+          {/* {menuOpen && (
+                <> */}
+          <div
+            id='mobileMenu'
+            className='ease-in-out duration-300'
+            style={{
+              position: 'absolute',
+              top: '60px',
+              left: menuOpen ? '0' : '100%',
+              right: '0',
+              bottom: '0',
+              height: '100%',
+              width: '100%',
+              zIndex: menuOpen ? '3' : '0',
+            }}
+          >
+            <Menu fn={toggleMenu} />
+          </div>
+          {/* </>
+              )} */}
+        </div>
+      </nav>
     </>
   );
 };
