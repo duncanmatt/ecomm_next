434ff59e8c2642e06c3a916fb36f41c8ce93c233
Matt Duncan <matt@matts-mbp.myfiosgateway.com>
2023-08-22 06:02:11 -0400

    change menu transition and add search transition

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -36,7 +36,7 @@ const Search = ({ active }: Status) => {
         transform: active ? 'translateY(0)' : 'translateY(-100%)',
         zIndex: active ? '60' : '0',
       }}
-      className='fixed top-0 left-0 h-full w-full right-0 overflow-y-hidden bg-[rgba(252,252,252,0.24)] bg-scroll bg-clip-padding backdrop-filter backdrop-blur-[150px] backdrop-contrast-100 saturate-[1.8] '
+      className='fixed top-0 left-0 h-full w-full right-0 overflow-y-hidden bg-[rgba(252,252,252,0.24)] bg-scroll bg-clip-padding backdrop-filter backdrop-blur-[150px] backdrop-contrast-100 saturate-[1.8] transition-[transform] ease-in'
     >
       <div className='relative px-1rem'>
         <div className='flex h-[54px] items-center justify-between w-full'>
@@ -54,7 +54,7 @@ const Search = ({ active }: Status) => {
         </div>
         <div>
           <div>
-            <div className='p-1rem'>
+            <div className='py-1rem'>
               <div style={{}} className='flex flex-col'>
                 <SearchScroll>
                   <SearchList filteredProducts={filteredProducts} />
